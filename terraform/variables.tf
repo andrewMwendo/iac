ff1785bfede558c42522d9347ee051d18625d272
Andrew Mwendo <mwendoandre@gmail.com>
2024-07-02 15:12:48 +0200

    updated terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-04b70fa74e45c3917" // Replace with the latest AMI ID for your region
+  default     = "ami-0eac975a54dfee8cb" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
